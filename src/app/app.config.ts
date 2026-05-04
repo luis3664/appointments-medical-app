8850032b390b54cf556bce143cb819fce53d532b
Luis Lopez <luis.lopez3664mail.com>
2026-05-04 10:28:27 -0300

    arreglando version de angular

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,25 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+// 🔥 Firebase
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
+// 🔥 environment
+import { environment } from '../environments/environment';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+
+    // 🔥 Firebase config
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
+  ]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyDKjyqtrKNFFwTl8z2qUUHh-bZJ8o4dx1s",
+        authDomain: "isi-lopez.firebaseapp.com",
+        projectId: "isi-lopez",
+        storageBucket: "isi-lopez.firebasestorage.app",
+        messagingSenderId: "616436511741",
+        appId: "1:616436511741:web:bef7caf572a2b6fcea5f94",
+        measurementId: "G-EX0Z4QBJLM"
+    }
+}
