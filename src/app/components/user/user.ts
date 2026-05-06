19c647208b3815ee38b6e2d7fa2ed3dd7da949b7
Luis Lopez <luis.lopez3664mail.com>
2026-05-05 21:49:38 -0300

    component de user para ver turnos y eliminarlos

--- src/app/components/user/user.ts
+++ src/app/components/user/user.ts
@@ -1,11 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppointmentService } from '../../services/appointment.service';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  standalone: true,
   templateUrl: './user.html',
-  styleUrl: './user.css',
+  styleUrl: './user.css'
 })
-export class User {
+export class User implements OnInit {
 
+  appointments: any[] = [];
+
+  userName: string = '';
+  userEmail: string = '';
+
+  constructor(
+    private appt: AppointmentService,
+    private authService: AuthService
+  ) {}
+
+  async ngOnInit() {
+
+    const userData = await this.authService.getCurrentUserData();
+
+    if (userData) {
+      this.userName = userData['name'];
+      this.userEmail = userData['email'];
+    }
+
+    this.appointments = await this.appt.getUserAppointments();
+  }
+
+  // 🔥 determina si ya pasó el turno
+  isPast(a: any): boolean {
+    return a.timestamp < Date.now();
+  }
+
+  // 🔥 botón cancelar
+  async cancelar(a: any) {
+
+    if (this.isPast(a)) return;
+
+    const confirmacion = confirm('¿Seguro que quieres cancelar este turno?');
+
+    if (!confirmacion) return;
+
+    try {
+      await this.appt.cancelAppointment(a);
+      
+      // 🔥 actualizar UI sin recargar
+      this.appointments = this.appointments.filter(x => x !== a);
+
+      alert('Turno cancelado');
+    } catch (e) {
+      console.error(e);
+      alert('Error al cancelar');
+    }
+  }
 }
