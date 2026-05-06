0f2afbcbcb04cda02f546ac5a1a30e3c0f222572
Luis Lopez <luis.lopez3664mail.com>
2026-05-06 08:04:38 -0300

    agregando roles y component admin

---
+++ src/app/components/admin/admin.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { AppointmentService } from '../../services/appointment.service';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-admin',
+  standalone: true,
+  templateUrl: './admin.html',
+  styleUrl: './admin.css'
+})
+export class AdminComponent implements OnInit {
+
+  appointments: any[] = [];
+
+  constructor(
+    private appt: AppointmentService,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  async ngOnInit() {
+
+    const user = await this.authService.getCurrentUserData();
+
+    if (!user || user['role'] !== 'admin') {
+      this.router.navigate(['/index']);
+      return;
+    }
+
+    this.appointments = await this.appt.getAllAppointments();
+  }
+
+  isPast(a: any) {
+    return a.timestamp < Date.now();
+  }
+
+  async cancelar(a: any) {
+    if (this.isPast(a)) return;
+
+    await this.appt.cancelAppointment(a);
+
+    this.appointments = this.appointments.filter(x => x.id !== a.id);
+  }
+}
